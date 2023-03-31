f17abf9fb256b594500cc25e16cbae62b37fd153
rsdy <p@symmetree.dev>
2023-03-31 16:46:09 +0100

    Move iterator code to separate module

--- server/bleep/src/repo.rs
+++ server/bleep/src/repo.rs
@@ -11,9 +11,11 @@ use utoipa::ToSchema;
 
 use crate::{
     ctags, indexes,
-    language::{get_language_info, LanguageInfo},
     state::{get_relative_path, pretty_write_file},
 };
+
+pub(crate) mod iterator;
+use iterator::language;
 
 pub(crate) type FileCache = Arc<scc::HashMap<PathBuf, FreshValue<String>>>;
 
@@ -316,7 +318,7 @@ fn get_unix_time(time: SystemTime) -> u64 {
 pub struct RepoMetadata {
     pub last_commit_unix_secs: u64,
     pub symbols: ctags::SymbolMap,
-    pub langs: LanguageInfo,
+    pub langs: language::LanguageInfo,
 }
 
 async fn get_repo_metadata(repo_disk_path: &PathBuf) -> Arc<RepoMetadata> {
@@ -351,7 +353,7 @@ async fn get_repo_metadata(repo_disk_path: &PathBuf) -> Arc<RepoMetadata> {
     RepoMetadata {
         last_commit_unix_secs: repo,
         symbols: ctags::get_symbols(repo_disk_path, exclude_langs).await,
-        langs: get_language_info(repo_disk_path),
+        langs: language::aggregate(repo_disk_path),
     }
     .into()
 }
